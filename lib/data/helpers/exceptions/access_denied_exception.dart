f36b2f08e37f0f5a7e7167dc1cdad2dcf17ad443
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-07-22 14:17:23 -0300

    add custom exceptions

---
+++ lib/data/helpers/exceptions/access_denied_exception.dart
@@ -0,0 +1,1 @@
+class AccessDeniedException implements Exception {}
---
+++ lib/data/helpers/exceptions/exceptions.dart
@@ -0,0 +1,4 @@
+export 'access_denied_exception.dart';
+export 'invalid_credentials_exception.dart';
+export 'server_exception.dart';
+export 'unexpected_exception.dart';

c17d0c88c129e8c332d287a848cfeeadb2943af5
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-07-22 13:23:50 -0300

    update Failure message to be final

--- lib/domain/helpers/failure/server_failure.dart
+++ lib/domain/helpers/failure/server_failure.dart
@@ -1,7 +1,7 @@
 import 'failure.dart';
 
 class ServerFailure extends Failure {
-  String _message = 'Ops! Something wrong with the server.';
+  final String _message = 'Ops! Something wrong with the server.';
   String get message => _message;
   @override
   List<Object?> get props => [];

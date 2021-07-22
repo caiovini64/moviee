09211757b25995686b676f5aca06acecf5016deb
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-07-22 12:37:24 -0300

    add MovieDetails repository

---
+++ lib/domain/repositories/movie_details_repository.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:moviee/domain/entities/entities.dart';
+import 'package:moviee/domain/helpers/helpers.dart';
+
+abstract class IMovieDetailsRepository {
+  Future<Either<Failure, MovieEntity>> getMovieDetails(MovieEntity movie);
+}

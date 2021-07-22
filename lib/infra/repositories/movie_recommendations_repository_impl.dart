b97ae0092d7ffab25dfeebc4cd720f25cd6831d7
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-07-22 15:30:55 -0300

    ensure MovieRecommendationsRepository returns a ServerFailure when datasource throws a Server Exception

--- lib/infra/repositories/movie_recommendations_repository_impl.dart
+++ lib/infra/repositories/movie_recommendations_repository_impl.dart
@@ -1,8 +1,10 @@
 import 'package:moviee/domain/helpers/failure/failure.dart';
 import 'package:moviee/domain/entities/movie_entity.dart';
 import 'package:dartz/dartz.dart';
+import 'package:moviee/domain/helpers/failure/failures.dart';
 import 'package:moviee/domain/repositories/repositories.dart';
 import 'package:moviee/infra/datasources/datasources.dart';
+import 'package:moviee/infra/helpers/exceptions/exceptions.dart';
 
 class MovieRecommendationsRepository
     implements IMovieRecommendationsRepository {
@@ -12,7 +14,11 @@ class MovieRecommendationsRepository
   @override
   Future<Either<Failure, List<MovieEntity>>> getMovieRecommendations(
       MovieEntity params) async {
-    final result = await datasource.getMovieRecommendations(params);
-    return Right(result);
+    try {
+      final result = await datasource.getMovieRecommendations(params);
+      return right(result);
+    } on ServerException {
+      return Left(ServerFailure());
+    }
   }
 }
--- test/infra/repositories/movie_recommendations_repository_impl_test.dart
+++ test/infra/repositories/movie_recommendations_repository_impl_test.dart
@@ -3,7 +3,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
 import 'package:moviee/domain/entities/entities.dart';
+import 'package:moviee/domain/helpers/failure/failures.dart';
 import 'package:moviee/infra/datasources/datasources.dart';
+import 'package:moviee/infra/helpers/exceptions/exceptions.dart';
 import 'package:moviee/infra/repositories/repositories.dart';
 
 import '../../mocks.dart';
@@ -32,6 +34,16 @@ void main() {
         .thenAnswer((_) async => kMovieModelList);
     final result = await repository.getMovieRecommendations(kMovieEntity);
     expect(result, Right(kMovieModelList));
+    verify(() => datasource.getMovieRecommendations(any()));
+  });
+
+  test(
+      'should return a ServerFailure when calls to datasource throws a Server Exception',
+      () async {
+    when(() => datasource.getMovieRecommendations(any()))
+        .thenThrow(ServerException());
+    final result = await repository.getMovieRecommendations(kMovieEntity);
+    expect(result, Left(ServerFailure()));
     verify(() => datasource.getMovieRecommendations(any()));
   });
 }

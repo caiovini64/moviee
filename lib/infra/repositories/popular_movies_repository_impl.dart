c44caf0f444b489671421c7157a4e871baed142c
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-07-23 07:48:23 -0300

    ensure PopularMoviesRepository returns a ServerFailure when datasource throws a Server Exception

--- lib/infra/repositories/popular_movies_repository_impl.dart
+++ lib/infra/repositories/popular_movies_repository_impl.dart
@@ -1,8 +1,10 @@
 import 'package:moviee/domain/helpers/failure/failure.dart';
 import 'package:moviee/domain/entities/movie_entity.dart';
 import 'package:dartz/dartz.dart';
+import 'package:moviee/domain/helpers/failure/failures.dart';
 import 'package:moviee/domain/repositories/repositories.dart';
 import 'package:moviee/infra/datasources/datasources.dart';
+import 'package:moviee/infra/helpers/exceptions/exceptions.dart';
 
 class PopularMoviesRepository implements IPopularMoviesRepository {
   final IPopularMoviesDatasource datasource;
@@ -10,7 +12,11 @@ class PopularMoviesRepository implements IPopularMoviesRepository {
 
   @override
   Future<Either<Failure, List<MovieEntity>>> getPopularMovies() async {
-    final result = await datasource.getPopularMovies();
-    return Right(result);
+    try {
+      final result = await datasource.getPopularMovies();
+      return Right(result);
+    } on ServerException {
+      return Left(ServerFailure());
+    }
   }
 }
--- test/infra/repositories/popular_movies_repository_impl_test.dart
+++ test/infra/repositories/popular_movies_repository_impl_test.dart
@@ -1,9 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:moviee/domain/helpers/failure/failures.dart';
 
 import 'package:moviee/domain/repositories/repositories.dart';
 import 'package:moviee/infra/datasources/datasources.dart';
+import 'package:moviee/infra/helpers/exceptions/exceptions.dart';
 import 'package:moviee/infra/repositories/repositories.dart';
 
 import '../../mocks.dart';
@@ -26,6 +28,15 @@ main() {
         .thenAnswer((_) async => kMovieModelList);
     final result = await repository.getPopularMovies();
     expect(result, Right(kMovieModelList));
+    verify(() => datasource.getPopularMovies());
+  });
+
+  test(
+      'should return a ServerFailure when calls to datasource throws a ServerException',
+      () async {
+    when(() => datasource.getPopularMovies()).thenThrow(ServerException());
+    final result = await repository.getPopularMovies();
+    expect(result, Left(ServerFailure()));
     verify(() => datasource.getPopularMovies());
   });
 }

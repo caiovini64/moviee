52bade5d10e4b37b1e74b8c11d34ccbf9fa6da02
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-07-27 08:18:39 -0300

    removes responsive_framework

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:moviee/infra/dependencies/injection_container.dart'
     as dependencies;
 import 'package:moviee/presenter/helpers/helpers.dart';
 import 'package:moviee/presenter/routes/app_pages.dart';
-import 'package:responsive_framework/responsive_framework.dart';
 
 import 'presenter/pages/home/home_page.dart';
 
@@ -17,19 +16,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      builder: (context, widget) {
-        return ResponsiveWrapper.builder(widget,
-            minWidth: 480,
-            defaultScale: true,
-            breakpoints: [
-              ResponsiveBreakpoint.resize(480, name: MOBILE),
-              ResponsiveBreakpoint.autoScale(800, name: TABLET),
-              ResponsiveBreakpoint.resize(1000, name: DESKTOP),
-            ],
-            background: Container(
-              color: appTheme().backgroundColor,
-            ));
-      },
       title: 'Just Movie it',
       debugShowCheckedModeBanner: false,
       theme: appTheme(),

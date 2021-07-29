3487c6178d8ad223104146439664c9660a03d210
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-07-29 08:17:19 -0300

    removes unecessary routes

--- lib/presenter/routes/app_routes.dart
+++ lib/presenter/routes/app_routes.dart
@@ -1,9 +1,5 @@
-import 'package:moviee/presenter/pages/details/details_page.dart';
 import 'package:moviee/presenter/pages/home/home_page.dart';
-import 'package:moviee/presenter/pages/search/search_page.dart';
 
 abstract class Routes {
   static const HOME = HomePage.route;
-  static const SEARCH = SearchPage.route;
-  static const DETAILS = DetailsPage.route;
 }

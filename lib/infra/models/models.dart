84ad95873bdf3876c02f48918c980f283e5b18a2
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-07-30 09:57:29 -0300

    creates UserModel

--- lib/infra/models/models.dart
+++ lib/infra/models/models.dart
@@ -2,3 +2,4 @@ export 'movie_model.dart';
 export 'genres_model.dart';
 export 'movie_details_model.dart';
 export 'http_response_model.dart';
+export 'user_model.dart';
---
+++ lib/infra/models/user_model.dart
@@ -0,0 +1,23 @@
+import 'package:moviee/domain/entities/entities.dart';
+
+class UserModel extends UserEntity {
+  final String email;
+  final String id;
+
+  UserModel({required this.email, required this.id})
+      : super(email: email, id: id);
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      email: json['email'],
+      id: json['id'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'email': email,
+      'id': id,
+    };
+  }
+}

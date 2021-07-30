72e91477985a75da27e772671aa5eb8ac077a5f0
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-07-30 09:21:47 -0300

    update TMDBEndpoints

--- lib/external/helpers/endpoints/tmdb_endpoints.dart
+++ lib/external/helpers/endpoints/tmdb_endpoints.dart
@@ -2,9 +2,9 @@ import 'api_key.dart';
 
 class TMDBEndpoints {
   static String movieList(String category) =>
-      '$base_url/movie/$category?api_key=$api_key';
+      '$base_url/$category?api_key=$api_key';
   static String movieWithParams(String category, String params) =>
-      '$base_url/movie/$params/$category?api_key=$api_key';
+      '$base_url/$params/$category?api_key=$api_key';
   static String movieDetails(String movieId) =>
-      '$base_url/movie/$movieId?api_key=$api_key';
+      '$base_url/$movieId?api_key=$api_key';
 }

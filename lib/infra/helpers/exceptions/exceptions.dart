936aa4e23f0d397bb95f124d0437d178d85c485a
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-07-30 10:12:50 -0300

    creates SignupException

--- lib/infra/helpers/exceptions/exceptions.dart
+++ lib/infra/helpers/exceptions/exceptions.dart
@@ -2,3 +2,4 @@ export 'access_denied_exception.dart';
 export 'invalid_credentials_exception.dart';
 export 'server_exception.dart';
 export 'unexpected_exception.dart';
+export 'signup_excepton.dart';
---
+++ lib/infra/helpers/exceptions/signup_excepton.dart
@@ -0,0 +1,1 @@
+class SignupException implements Exception {}

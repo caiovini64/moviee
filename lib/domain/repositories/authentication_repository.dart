e7f56b7efa5ec5e721da5f5007aa5fe9e51b366b
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-07-30 09:30:30 -0300

    creates abstract class AuthenticationRepository

---
+++ lib/domain/repositories/authentication_repository.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:moviee/domain/entities/entities.dart';
+import 'package:moviee/domain/helpers/helpers.dart';
+
+abstract class IAuthenticationRepository {
+  Future<Either<Failure, UserEntity>> signInWithEmail(
+      {String email, String password});
+}
